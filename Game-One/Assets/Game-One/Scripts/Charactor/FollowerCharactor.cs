b71e83eebac03a58524542186f84334193c3def5
aube.lee <leejjoo112@icloud.com>
2025-01-01 22:23:03 +0900

    캐릭터 follower 펫 추가

---
+++ Game-One/Assets/Game-One/Scripts/Charactor/FollowerCharactor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ONE
+{
+    public class FollowerCharactor : MonoBehaviour
+    {
+        public Transform followTarget;
+        public Vector3 offset;
+        private NavMeshAgent navAgent;
+
+
+        private void Awake()
+        {
+            navAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (followTarget != null)
+            {
+                Vector3 targetPosition = followTarget.position + offset;
+                navAgent.SetDestination(targetPosition);
+            }
+        }
+    }
+}
