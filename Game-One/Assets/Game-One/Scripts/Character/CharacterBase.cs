d0b299e8474291437b4caba7a9fb02130ee98e60
aube.lee <leejjoo112@icloud.com>
2025-01-06 23:55:12 +0900

    캐릭터 이동 애니메이션 NavAgent 연동

--- Game-One/Assets/Game-One/Scripts/Character/CharacterBase.cs
+++ Game-One/Assets/Game-One/Scripts/Character/CharacterBase.cs
@@ -8,9 +8,35 @@ namespace ONE
 {
     public class CharactorBase : MonoBehaviour
     {
+        void OnDrawGizmos()
+        {
+            if (navAgent && navAgent.path.corners.Length > 0)
+            {
+                for (int i = 0; i < navAgent.path.corners.Length; i++)
+                {
+                    if (i == 0)
+                    {    // 시작점
+                        Gizmos.color = new Color(1f, 0f, 0f, 0.7f); // 빨강강
+                    }
+                    else if (i == navAgent.path.corners.Length - 1)
+                    {   // 도착점
+                        Gizmos.color = new Color(0f, 1f, 0f, 0.7f); // 녹색색
+                    }
+                    else
+                    {   // 중간 지점들
+                        Gizmos.color = new Color(0f, 0f, 1f, 0.7f); // 파랑
+                    }
+                    Gizmos.DrawSphere(navAgent.path.corners[i], 0.5f);
+                }
+
+                // navAgent.steeringTarget => corners[1]
+                Gizmos.color = new Color(1f, 1f, 0f, 0.7f);
+                Gizmos.DrawCube(navAgent.steeringTarget, Vector3.one * 0.7f);
+
+            }
+        }
 
         public float moveSpeed = 3.0f;
-
 
         public bool IsEquip { get; private set; }
         public bool IsRun { get; private set; }
@@ -18,10 +44,9 @@ namespace ONE
         private Animator animator;
         private NavMeshAgent navAgent;
 
-        // private Vector2 movementInput;
+
         private Vector3 inputDirection;
-        // private float smoothHorizontal;
-        // private float smoothVertical;
+
 
         public float currentHP;
         public float maxHP;
@@ -32,6 +57,8 @@ namespace ONE
         {
             animator = GetComponent<Animator>();
             navAgent = GetComponent<NavMeshAgent>();
+
+            navAgent.updatePosition = false;
             navAgent.updateRotation = false;
         }
 
@@ -46,8 +73,9 @@ namespace ONE
 
         private void Update()
         {
-            RotateToNextPoint();
             UpdateAnimationParamter();
+
+            SynchronizeAnimatorAndAgent();
 
             if (isDashing)
             {
@@ -62,14 +90,48 @@ namespace ONE
                 }
             }
 
-            // smoothHorizontal = Mathf.Lerp(smoothHorizontal, inputDirection.x, Time.deltaTime * 10f);
-            // smoothVertical = Mathf.Lerp(smoothVertical, inputDirection.z, Time.deltaTime * 10f);
-
             animator.SetFloat("Equip Blend", IsEquip ? 1.0f : 0.0f);
             animator.SetFloat("Running Blend", IsRun ? 1.0f : 0.0f);
-            // animator.SetFloat("Horizontal", smoothHorizontal);
-            // animator.SetFloat("Vertical", smoothVertical);
-            animator.SetFloat("Magnitude", navAgent.desiredVelocity.magnitude);
+
+        }
+
+        private Vector2 velocity;
+        private Vector2 smoothDeltaPosition;
+
+        private void SynchronizeAnimatorAndAgent()
+        {
+            Vector3 worldDeltaPosition = navAgent.nextPosition - transform.position;
+            worldDeltaPosition.y = 0f;
+
+            float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+            float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+            Vector2 deltaPosition = new Vector2(dx, dy);
+
+            float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+            smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+            velocity = smoothDeltaPosition / Time.deltaTime;
+            if (navAgent.remainingDistance <= navAgent.stoppingDistance)
+            {
+                velocity = Vector2.Lerp(Vector2.zero, velocity, navAgent.remainingDistance / navAgent.stoppingDistance);
+            }
+
+            bool shouldMove = velocity.magnitude > 0.5f && navAgent.remainingDistance > navAgent.stoppingDistance;
+
+            if (shouldMove)
+            {
+                Vector3 direction = (navAgent.path.corners[1] - transform.position).normalized;
+                direction.y = 0;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * 10f);
+            }
+            animator.SetBool("IsMoving", shouldMove);
+            animator.SetFloat("Magnitude", velocity.magnitude > 0f ? velocity.magnitude : 0f);
+
+            float deltaMagnitude = worldDeltaPosition.magnitude;
+            if (deltaMagnitude > navAgent.radius * 0.5f)
+            {
+                transform.position = Vector3.Lerp(animator.rootPosition, navAgent.nextPosition, smooth);
+            }
 
         }
 
@@ -78,44 +140,12 @@ namespace ONE
             inputDirection.z = navAgent.desiredVelocity.magnitude > 0f ? 1f : 0f;
         }
 
-        public void RotateToNextPoint()
-        {
-            // Debug.Log("Nav Agent Path Data");
-            // for (int i = 0; i < navAgent.path.corners.Length; i++)
-            // {
-            //     Debug.Log($"Path Corners [{i}] : {navAgent.path.corners[i]}");
-            // }
-            // Debug.Log("Nav Agent Path Data End");
-
-            if (navAgent.path.corners.Length > 1)
-            {
-                Vector3 direction = (navAgent.path.corners[1] - transform.position).normalized;
-                direction.y = 0;
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * 10f);
-            }
-        }
-
         public void SetDestination(Vector3 destination)
         {
             if (isDashing || isAttacking)
                 return;
 
             navAgent.SetDestination(destination);
-        }
-
-        private void OnDrawGizmos()
-        {
-            if (navAgent)
-            {
-                Gizmos.color = new Color(0f, 1f, 0f, 0.7f);
-
-                for (int i = 0; i < navAgent.path.corners.Length; i++)
-                {
-                    Gizmos.DrawSphere(navAgent.path.corners[i], 0.5f);
-                }
-
-            }
-
         }
 
         public float dashDuration = 1.5f;
