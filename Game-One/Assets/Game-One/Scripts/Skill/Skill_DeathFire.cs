8052cde3e9b8bd8c3921bee2cb4c90e55ca5c2b5
aube.lee <leejjoo112@icloud.com>
2025-01-22 11:53:58 +0900

    제자리에서 sphereCast 데미지 적용하기

--- Game-One/Assets/Game-One/Scripts/Skill/Skill_DeathFire.cs
+++ Game-One/Assets/Game-One/Scripts/Skill/Skill_DeathFire.cs
@@ -6,15 +6,48 @@ namespace ONE
 {
     public class Skill_DeathFire : SkillBase
     {
+
         public override void ExecuteSkill(CharacterBase actor)
         {
             // actor의 데이터가 필요한 경우 가져올 수 잇음
 
             //TODO: 스킬을 사용했을때 행동 구현
             actor.IsProgressingAction = true;
-            actor.ExecuteSkillAnimation(this.SkillData.SkillAnimationStateName);
+            actor.ExecuteSkillAnimation(SkillData.SkillAnimationStateName);
 
             CurrentCoolDown = SkillCoolDown;
+
+            actor.StartCoroutine(RepeatApplayDamage(actor, 3, 0.3f));
+        }
+
+        private IEnumerator RepeatApplayDamage(CharacterBase actor, int repeatCount, float interval)
+        {
+            for (int i = 0; i < repeatCount; i++)
+            {
+                ApplayDamage(actor);
+                yield return new WaitForSeconds(interval);
+            }
+        }
+
+        private void ApplayDamage(CharacterBase actor)
+        {
+            LayerMask mask = LayerMask.GetMask("Monster");
+            RaycastHit[] raycastHits = Physics.SphereCastAll(actor.transform.position, SkillData.Range, Vector3.up, 0, mask);
+
+            // 거리에 따라 배열을 정렬
+            System.Array.Sort(raycastHits, (x, y) => x.distance.CompareTo(y.distance));
+
+            if (raycastHits.Length > 0)
+            {
+                foreach (RaycastHit hit in raycastHits)
+                {
+                    Debug.Log(hit);
+                    if (hit.collider.transform.root.TryGetComponent(out IDamage damageInterface))
+                    {
+                        damageInterface.ApplyDamage(SkillData.Damage);
+                    }
+                }
+            }
         }
     }
 }
